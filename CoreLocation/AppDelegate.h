b03c1112a0e97cba2afab6f6568015f3fd3e1e45
Kelsey Pedersen <kelseypedersen@gmail.com>
2015-08-12 16:48:44 -0700

    Initial Commit

---
+++ CoreLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreLocation
+//
+//  Created by Kelsey Pedersen on 8/12/15.
+//  Copyright (c) 2015 Kelsey Pedersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreLocation
+//
+//  Created by Kelsey Pedersen on 8/12/15.
+//  Copyright (c) 2015 Kelsey Pedersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
